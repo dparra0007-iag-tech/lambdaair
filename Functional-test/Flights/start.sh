e1d81bd49ae59e6927c3b4c6b8dd22e6469b24a4
david-iaggbs <david.parra@iaggbs.com>
2019-12-24 12:38:00 +0100

    debug functional test

--- Functional-test/Flights/start.sh
+++ Functional-test/Flights/start.sh
@@ -3,7 +3,7 @@ set -e
 
 sleep 30
 
-newman run https://www.getpostman.com/collections/29c4e07a8d6d539a954e --reporters cli,junit --reporter-junit-export outputfile.xml --environment https://api.getpostman.com/environments/651996-b80ed237-9179-35ec-d8a1-35d574e118c8?apikey=100822fe2bd7454eb916c8ebdd4be266 --folder flights
+newman run https://www.getpostman.com/collections/29c4e07a8d6d539a954e --reporters cli,junit --reporter-junit-export outputfile.xml --environment https://api.getpostman.com/environments/651996-b80ed237-9179-35ec-d8a1-35d574e118c8?apikey=100822fe2bd7454eb916c8ebdd4be266 --folder functional/flights
 
 sed -i -e 's/<testcase/<testcase classname=\"CI.GLPLAMBDAAIR\"/g' outputfile.xml
 
